de966ba84965d5fa944ebed2b56fd757fe6c1aa6
tanup9539 <tanup9539@gmail.com>
2025-09-17 22:43:10 +0530

    modified

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router = express.Router()
 
 router.get('',(req,res)=>{
-    res.send("Kisaan-Mitra")
+    res.send("Kisaan-Mitra hain ham")
 })
 
 module.exports = router;
